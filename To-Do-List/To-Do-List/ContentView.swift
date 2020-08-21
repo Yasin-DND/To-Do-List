0b30aa6bbe7508650d88863ed1254859714df17f
Yasin-DND <55541231+Yasin-DND@users.noreply.github.com>
2020-08-22 00:12:29 +0500

    Add files via upload

---
+++ To-Do-List/To-Do-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do-List
+//
+//  Created by Mac on 22/08/2563 BE.
+//  Copyright © 2563 DND. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
